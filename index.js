61035010b60de4ae48634035cf8c28971f20d3c1
ArielGol <ariel00golubickas@gmail.com>
2021-06-19 14:50:55 -0300

    elimine los elementos li antiguos y cree nuevos li

--- index.js
+++ index.js
@@ -25,6 +25,26 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  /*Dos métodos para eliminar los elementos hijos
+    while (listaEl.firstChild) {
+    listaEl.removeChild(listaEl.firstChild);
+    ó
+  }*/
+  listaEl.innerHTML = "";
+
+  function crearUlEl(array) {
+    var list = document.querySelector(".lista");
+    for (let i = 0; i < array.length; i++) {
+      var item = document.createElement("li");
+      item.appendChild(document.createTextNode(array[i].tema));
+      item.className += array[i].class;
+      list.appendChild(item);
+    }
+    return list;
+  }
+  crearUlEl(cosasQueAprendimos);
+}
 
 main();
